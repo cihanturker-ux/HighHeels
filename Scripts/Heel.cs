173fed7fb60997be40522e79a39ca4a30b1e900d
ucaktas <ucaktas@gmail.com>
2021-03-20 12:20:18 +0300

    Add files via upload

---
+++ Scripts/Heel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heel : MonoBehaviour
+{  
+        public Transform parent;
+        public Rigidbody rb;
+        private Joint theJoint;
+        public GameObject personPlayer;
+        private PlayerController playerControllerScript;
+     void Start() {
+        theJoint = GetComponent<FixedJoint>(); // character joint
+        playerControllerScript = personPlayer.GetComponent<PlayerController>();
+
+    }
+    private void OnCollisionEnter(Collision other) {
+        if(other.gameObject.CompareTag("Obstacle")){
+            playerControllerScript.childs.Remove(gameObject);
+            theJoint.connectedBody = null;
+            rb.useGravity=true;
+            
+        }
+        if (other.gameObject.CompareTag("Player")){
+            theJoint.connectedBody = rb;
+            playerControllerScript.childs.Add(gameObject);
+
+        }
+        if (other.gameObject.CompareTag("Heel")){
+            rb.useGravity=false;
+            parent.transform.position += new Vector3(0,0.1f,0); // topuk boyuna göre ayarlanacak
+            theJoint.connectedBody =rb;
+            playerControllerScript.childs.Add(gameObject);        
+        }
+
+
+    }
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Transform parent;
+    public Rigidbody rb;
+    public List<GameObject> childs = new List<GameObject>();
+    public int heelNumber=1;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>(); 
+
+    } 
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.CompareTag("Heel") && heelNumber==1){
+            parent.transform.position += new Vector3(0,0.2f,0); // topuk boyuna göre ayarlanacak
+            heelNumber=2;
+        }
+        if(col.gameObject.CompareTag("Obstacle")){
+       
+        }
+    }
+ void Update() {
+   
+}
+    void FixedUpdate()
+    {
+      
+    }
+}
