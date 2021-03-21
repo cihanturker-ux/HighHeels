67f757d9037555541ae1a208446a257c4ba7b07b
ucaktas <ucaktas@gmail.com>
2021-03-21 22:02:01 +0300

    Update PlayerController.cs

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,15 +8,25 @@ public class PlayerController : MonoBehaviour
     public Rigidbody rb;
     public List<GameObject> childs = new List<GameObject>();
     public int heelNumber=1;
+
+    protected Joystick joystick;
+    protected JoyButton joyButton;
+
+    private Vector3 movement;
+    private int speed = 1;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>(); 
+        joystick=FindObjectOfType<Joystick>();
+        joyButton=FindObjectOfType<JoyButton>();
+
 
     } 
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.CompareTag("Heel") && heelNumber==1){
-            parent.transform.position += new Vector3(0,0.2f,0); // topuk boyuna göre ayarlanacak
+            parent.transform.position += new Vector3(col.gameObject.transform.position.x,0.2f,col.gameObject.transform.position.z); // topuk boyuna göre ayarlanacak
             heelNumber=2;
         }
         if(col.gameObject.CompareTag("Obstacle")){
@@ -24,10 +34,13 @@ public class PlayerController : MonoBehaviour
         }
     }
  void Update() {
-   
+    movement = new Vector3(joystick.Horizontal * 1f, rb.velocity.y , joystick.Vertical * 1f);
+    movement = movement.normalized * speed * Time.deltaTime;
+    rb.MovePosition (rb.position + movement);
 }
-    void FixedUpdate()
+     void FixedUpdate()
     {
-      
+     rb.velocity= new Vector3(0,0,2f); 
+     
     }
 }
