9c59fc57c4de8597ca3c358d55f140f261ba51d5
itsoguzhan <oguzhan.delibas@hbv.edu.tr>
2021-03-24 20:34:39 +0300

    Some Scripts

---
+++ Scripts/O.D/AnimatePlayer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerController))]
+
+public class AnimatePlayer : MonoBehaviour
+{
+    public static AnimatePlayer instance;
+    
+    Rigidbody rb;
+    [SerializeField] float speed = 5;
+    Vector3 stepPosition;
+    GameObject player;
+
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Physics.gravity = new Vector3(0, -500.0F, 0);
+        player = GameObject.FindGameObjectWithTag("Player");
+        print(Screen.currentResolution);
+
+    }
+    
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector3(0, 0, speed);
+        
+    }
+    
+    public void PlayerPosUp()
+    {
+        Vector3 playerpos = transform.localPosition;
+        playerpos.y += .5f;
+        transform.localPosition = playerpos;
+    }
+    
+}
---
+++ Scripts/O.D/HeelController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeelController : MonoBehaviour
+{
+    private GameObject leftParent, rightParent;
+    public static int heel = 1;
+    void Awake()
+    {
+        leftParent = GameObject.FindGameObjectWithTag("LeftParent");
+        rightParent = GameObject.FindGameObjectWithTag("RightParent");
+    }
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Heel")
+        {
+            leftParent.transform.GetChild(heel).gameObject.SetActive(true);
+            rightParent.transform.GetChild(heel).gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            AnimatePlayer.instance.PlayerPosUp();
+
+
+            heel++;
+        }
+    } 
+}
